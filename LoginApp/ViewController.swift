b63d337cb897f7e8b9fdfc36a35c286b08a6f5ba
山口拓 <taku0527@yamaguchitakunoMacBook-Air.local>
2020-08-06 16:51:14 +0900

    First commit

---
+++ LoginApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginApp
+//
+//  Created by 山口拓 on 2020/08/04.
+//  Copyright © 2020 山口拓. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
